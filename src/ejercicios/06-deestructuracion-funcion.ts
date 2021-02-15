735628322ceb4557aea6f68c9d965b0ea4586970
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-02-15 12:18:56 -0600

    Finish course

--- src/ejercicios/06-deestructuracion-funcion.ts
+++ src/ejercicios/06-deestructuracion-funcion.ts
@@ -1,7 +1,11 @@
-interface Producto {
+export interface Producto {
     desc: string;
     precio: number;
 }
+// interface Producto {
+//     desc: string;
+//     precio: number;
+// }
 const telefono:Producto = {
     desc: 'Nokia',
     precio: 150
@@ -11,7 +15,14 @@ const tableta:Producto = {
     precio: 350
 }
 
-function calculaISV (productos: Producto[]):[number,number] {
+// function calculaISV (productos: Producto[]):[number,number] {
+//     let total = 0;
+//     productos.forEach(({ precio})=>{
+//         total += precio
+//     })
+//     return [total, total *0.15]
+// }
+export function calculaISV (productos: Producto[]):[number,number] {
     let total = 0;
     productos.forEach(({ precio})=>{
         total += precio
